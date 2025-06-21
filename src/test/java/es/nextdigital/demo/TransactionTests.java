d40f3920c562ecfa91d3206ae7245371b1467047
Ale Verano <averano1998@gmail.com>
2025-06-21 15:17:10 +0200

    removed unused import

--- src/test/java/es/nextdigital/demo/TransactionTests.java
+++ src/test/java/es/nextdigital/demo/TransactionTests.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
